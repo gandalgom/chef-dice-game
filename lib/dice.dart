720d297844c0ce0910fdb631b3c0b462276f9e1d
Max Yoo <devbear74@gmail.com>
2022-06-21 11:52:10 +0900

    주사위 페이지 디자인 구현

--- lib/dice.dart
+++ lib/dice.dart
@@ -6,11 +6,44 @@ class Dice extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.redAccent,
       appBar: AppBar(
+        backgroundColor: Colors.redAccent,
         title: const Text('Dice page'),
       ),
-      body: const Center(
-        child: Text('Dice game page'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Image.asset('images/dice1.png')
+                  ),
+                  const SizedBox(width: 20.0),
+                  Expanded(
+                    child: Image.asset('images/dice4.png')
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 60.0),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orangeAccent,
+                minimumSize: const Size(100.0, 60.0),
+              ),
+              child: const Icon(
+                Icons.play_arrow,
+                color: Colors.white,
+                size: 50.0,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
