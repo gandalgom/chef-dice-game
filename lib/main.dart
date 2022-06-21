844769026ed194993525008a11d28ce995fb2bb0
Max Yoo <devbear74@gmail.com>
2022-06-21 11:10:52 +0900

    빈 공간 탭하여 키보드 내리는 기능 구현

--- lib/main.dart
+++ lib/main.dart
@@ -50,80 +50,85 @@ class _LoginState extends State<Login> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const Padding(padding: EdgeInsets.only(top: 50.0)),
-            const Center(
-              child: Image(
-                image: AssetImage('images/chef.gif'),
-                width: 170.0,
-                height: 190.0,
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Padding(padding: EdgeInsets.only(top: 50.0)),
+              const Center(
+                child: Image(
+                  image: AssetImage('images/chef.gif'),
+                  width: 170.0,
+                  height: 190.0,
+                ),
               ),
-            ),
-            Form(
-              child: Theme(
-                data: ThemeData(
-                  primaryColor: Colors.teal,
-                  inputDecorationTheme: const InputDecorationTheme(
-                    labelStyle: TextStyle(
-                      color: Colors.teal,
-                      fontSize: 15.0,
+              Form(
+                child: Theme(
+                  data: ThemeData(
+                    primaryColor: Colors.teal,
+                    inputDecorationTheme: const InputDecorationTheme(
+                      labelStyle: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.all(40.0),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: diceInputController,
+                          decoration: const InputDecoration(
+                            labelText: 'Enter "dice"',
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        TextField(
+                          controller: passwordInputController,
+                          decoration: const InputDecoration(
+                            labelText: 'Enter password',
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                        ),
+                        const SizedBox(height: 40.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (diceInputController.text == 'dice') {
+                              if (passwordInputController.text == '1234') {
+                                Navigator.push(context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const Dice()
+                                  )
+                                );
+                              } else {
+                                showSnackBar(context, 2);
+                              }
+                            } else {
+                              if (passwordInputController.text == '1234') {
+                                showSnackBar(context, 1);
+                              } else {
+                                showSnackBar(context, 0);
+                              }
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.orangeAccent,
+                            minimumSize: const Size(100.0, 50.0),
+                          ),
+                          child: const Icon(Icons.arrow_forward, size: 35.0),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-                child: Container(
-                  padding: const EdgeInsets.all(40.0),
-                  child: Column(
-                    children: [
-                      TextField(
-                        controller: diceInputController,
-                        decoration: const InputDecoration(
-                          labelText: 'Enter "dice"',
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                      TextField(
-                        controller: passwordInputController,
-                        decoration: const InputDecoration(
-                          labelText: 'Enter password',
-                        ),
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                      ),
-                      const SizedBox(height: 40.0),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (diceInputController.text == 'dice') {
-                            if (passwordInputController.text == '1234') {
-                              Navigator.push(context,
-                                MaterialPageRoute(
-                                  builder: (context) => const Dice()
-                                )
-                              );
-                            } else {
-                              showSnackBar(context, 2);
-                            }
-                          } else {
-                            if (passwordInputController.text == '1234') {
-                              showSnackBar(context, 1);
-                            } else {
-                              showSnackBar(context, 0);
-                            }
-                          }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.orangeAccent,
-                          minimumSize: const Size(100.0, 50.0),
-                        ),
-                        child: const Icon(Icons.arrow_forward, size: 35.0),
-                      ),
-                    ],
-                  ),
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
